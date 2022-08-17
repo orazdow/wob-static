b25d1a564d101a1d0e70db426a8bc945d51a46fa
orazdow <ollierazdow@hotmail.com>
2022-08-17 15:18:04 -0700

    statemenu

--- src/components/menu.js
+++ src/components/menu.js
@@ -1,19 +1,26 @@
-import React from 'react';
+import React, {useState} from 'react';
 import {Link} from 'raviger';
 
-export default function Menu({links}){
+
+export default function Menu({links, l='', r='', u}){
+	const [sel, setSel] = useState(0); 
+	const cb = l||u ? e => setSel(e.target.dataset.val) : null;
+	const sp = l ? '\u00A0':'';
+	const ul = u ? ' menu-sel': '';
 
 	const items = links.map((el, i)=>{
-		return el ? i === 0 ? 
-			<Link href={el.path} key={'m'+i}>
-			<span className="menu-title">{el.name}
+		let name = i == sel ? l+el.name+r: sp+el.name+sp;
+		let cl = i == sel ? 'menu-sub'+ul: 'menu-sub';
+		return el ? i == 0 ? 
+			<Link href={el.path} key={'m'+i} onClick={cb}>
+			<span data-val={i} className={'menu-title'}>{el.name}
 			</span>
 			</Link> 
 			:
-			<Link href={el.path} key={'m'+i}>
-			<span className="menu-sub">{el.name}
+			<Link href={el.path} key={'m'+i} onClick={cb}>
+			<span data-val={i} className={cl}>{name}
 			</span>
-			</Link> : null;
+			</Link>: null;
 	});
 
 	return (<div className="menu">{items}</div>);
--- src/components/menurouter.js
+++ src/components/menurouter.js
@@ -6,7 +6,7 @@ export default function MenuRouter({className, basepath, routes, links}){
 	let route = useRoutes(routes, { basePath: basepath});
 	return (
 		<div className={className || ''}>
-		<Menu links={links}/>
+		<Menu links={links} l='[' r=']' u={false}/>
 			{route}
 		</div>
 	);
