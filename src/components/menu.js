bcac756c787ffa1abf4baf7e759f77b7217f1275
orazdow <ollierazdow@hotmail.com>
2022-08-17 15:19:21 -0700

    statemenu

--- src/components/menu.js
+++ src/components/menu.js
@@ -18,3 +18,27 @@ export default function Menu({links}){
 
 	return (<div className="menu">{items}</div>);
 }
+
+export function StateMenu({links, l='', r='', u}){
+	const [sel, setSel] = useState(0); 
+	const cb = l||u ? e => setSel(e.target.dataset.val) : null;
+	const sp = l ? '\u00A0':'';
+	const ul = u ? ' menu-sel': '';
+
+	const items = links.map((el, i)=>{
+		let name = i == sel ? l+el.name+r: sp+el.name+sp;
+		let cl = i == sel ? 'menu-sub'+ul: 'menu-sub';
+		return el ? i == 0 ? 
+			<Link href={el.path} key={'m'+i} onClick={cb}>
+			<span data-val={i} className={'menu-title'}>{el.name}
+			</span>
+			</Link> 
+			:
+			<Link href={el.path} key={'m'+i} onClick={cb}>
+			<span data-val={i} className={cl}>{name}
+			</span>
+			</Link>: null;
+	});
+
+	return (<div className="menu">{items}</div>);
+}
